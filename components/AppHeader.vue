2c4df5ca03d5dff35d22d3ff26fce978bed62c78
gabriel_floriano <gabriel@rotaexata.com.br>
2022-03-06 11:41:13 -0300

    create header component

---
+++ components/AppHeader.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="navbar">
+        <logo />
+    </div>
+</template>
+
+<script>
+import Logo from './Logo.vue';
+export default {
+  components: { Logo },
+}
+</script>
+
+<style lang="scss" src="../assets/scss/header.scss" scoped/>
