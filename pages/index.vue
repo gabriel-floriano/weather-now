4661fa17cebf7d819311530e10ab0a5150bbb0bc
gabriel_floriano <gabriel@rotaexata.com.br>
2022-03-06 11:41:40 -0300

    adding logo, loader and finishing the index of the project with the header

---
+++ pages/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <app-header />
+    <div class="main">
+      <card
+        v-for="city in cities"
+        :key="city"
+        :city="city"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import AppHeader from '~/components/AppHeader.vue';
+import Card from '~/components/Card.vue';
+export default {
+  components: {
+    Card,
+    AppHeader
+  },
+  data() {
+    return {
+      cities: ['Nuuk, GL', 'Urubici, BR', 'Nairobi, KE'],
+    }
+  },
+  head() {
+    return {
+      title: 'Weather Now',
+      meta: [
+          {
+            hid: 'main',
+            name: 'home',
+            content: 'See the Weather Now',
+          }
+      ],
+    }
+  },
+}
+
+</script>
